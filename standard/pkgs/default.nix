df6a9497956f3ce188db74b94a0cbbe03dc3d596
Carlo Cruz <me@carlocruz.com>
2024-05-26 00:09:15 +1000

    Updte modules

--- standard/pkgs/default.nix
+++ standard/pkgs/default.nix
@@ -1,6 +1,5 @@
-# Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
-{
-  # List your module files here
-  # my-module = import ./my-module.nix;
+# Custom packages, that can be defined similarly to ones from nixpkgs
+# You can build them using 'nix build .#example'
+pkgs: {
+  # example = pkgs.callPackage ./example { };
 }
