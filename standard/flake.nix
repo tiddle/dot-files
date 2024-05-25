d61e48ebdc0458eae8e776f593077af472e86e7c
Carlo Cruz <me@carlocruz.com>
2024-05-25 23:59:31 +1000

    Updte modules

--- standard/flake.nix
+++ standard/flake.nix
@@ -41,7 +41,7 @@
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
-    overlays = import ./overlays {inherit inputs;};
+    #overlays = import ./overlays {inherit inputs;};
     # Reusable nixos modules you might want to export
     # These are usually stuff you would upstream into nixpkgs
     nixosModules = import ./modules/nixos;
