f847aa289b13b0e8f48d964add9b20f0e709bfb1
Carlo Cruz <me@carlocruz.com>
2024-05-26 21:44:09 +1000

    Update configs

--- standard/home-manager/home.nix
+++ standard/home-manager/home.nix
@@ -13,6 +13,7 @@
   # You can import other home-manager modules here
   imports = [
     ./alacritty.nix
+    ./firefox.nix
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
 
--- standard/home-manager/programs.nix
+++ standard/home-manager/programs.nix
@@ -1,7 +1,3 @@
-{ 
-  pkgs,
-  ...
-}: 
 {
   home-manager = {
     enable = true;
@@ -25,61 +21,6 @@
         enable = true;
         plugins = ["git" "thefuck" ];
         theme = "bira";
-    };
-  };
-  firefox = {
-    enable = true;
-    profiles.default = {
-      isDefault = true;
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-        bitwarden
-      ];
-      settings = {
-        "beacon.enabled" = false;
-        "browser.contentblocking.category" = "strict";
-        "browser.display.background_color" = "#c5c8c6";
-        "browser.display.foreground_color" = "#1d1f21";
-        "browser.download.dir" = "/home/carlo/Download";
-        "browser.safebrowsing.appRepURL" = "";
-        "browser.safebrowsing.malware.enabled" = false;
-        "browser.search.hiddenOneOffs" = "Google,Yahoo,Bing,Amazon.com,Twitter";
-        "browser.search.suggest.enabled" = false;
-        "browser.send_pings" = false;
-
-        "browser.startup.page" = 3;
-        "browser.tabs.closeWindowWithLastTab" = false;
-        "browser.uidensity" = 1; # Dense.
-        "browser.urlbar.placeholderName" = "DuckDuckGo";
-        "browser.urlbar.speculativeConnect.enabled" = false;
-        "dom.battery.enabled" = false;
-        "dom.event.clipboardevents.enabled" = false;
-        "dom.security.https_only_mode" = true;
-        "experiments.activeExperiment" = false;
-        "experiments.enabled" = false;
-        "experiments.supported" = false;
-        "extensions.pocket.enabled" = false;
-        "general.smoothScroll" = false;
-        "geo.enabled" = false;
-        "gfx.webrender.all" = true;
-        "layout.css.devPixelsPerPx" = "1";
-        "media.navigator.enabled" = false;
-        "media.video_stats.enabled" = false;
-        "network.IDN_show_punycode" = true;
-        "network.allow-experiments" = false;
-        "network.dns.disablePrefetch" = true;
-        "network.http.referer.XOriginPolicy" = 2;
-        "network.http.referer.XOriginTrimmingPolicy" = 2;
-        "network.http.referer.trimmingPolicy" = 1;
-        "network.prefetch-next" = false;
-        "permissions.default.shortcuts" = 2; # Don't steal my shortcuts!
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.donottrackheader.value" = 1;
-        "privacy.firstparty.isolate" = true;
-        "signon.rememberSignons" = false;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "widget.content.gtk-theme-override" = "Adwaita:light";
-      };
     };
   };
 }
