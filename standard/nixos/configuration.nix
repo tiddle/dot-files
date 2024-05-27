f072eb56f7a7cef75c1143afd9b9cf0811a1e523
Carlo Cruz <me@carlocruz.com>
2024-05-27 21:44:14 +1000

    Update configs

--- standard/nixos/configuration.nix
+++ standard/nixos/configuration.nix
@@ -45,7 +45,7 @@
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
-    ../desktop-environment/xfce.nix
+    ../desktop-environment/budgie.nix
     ./hardware-configuration.nix
   ];
 
