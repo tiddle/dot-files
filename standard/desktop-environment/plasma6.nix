3dfa650d523d800bd4bce7e5ba11b13e7f5869e8
Carlo Cruz <me@carlocruz.com>
2024-05-27 20:50:49 +1000

    Update configs

--- standard/desktop-environment/plasma6.nix
+++ standard/desktop-environment/plasma6.nix
@@ -10,8 +10,4 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    vlc
-  ];
 }
